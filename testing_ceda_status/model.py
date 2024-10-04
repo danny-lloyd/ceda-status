7eb9e517d8bfa1509ce7a55bcd6b1fa57e69979f
danny-lloyd <danny.lloyd@stfc.ac.uk>
2024-10-04 16:52:32 +0100

    Add docstrings and use SCREAMING_SNAKE_CASE

--- testing_ceda_status/model.py
+++ testing_ceda_status/model.py
@@ -8,23 +8,30 @@ INPUT_DATE_FORMAT = "%Y-%m-%dT%H:%M"
 
 
 def check_date_format(value: str) -> dt.datetime:
+    """Makes sure the datetime format matches what the MOTD and CEDA status page expect"""
     return dt.datetime.strptime(value, INPUT_DATE_FORMAT)
 
 
 class Status(Enum):
-    down = "down"
-    resolved = "resolved"
-    degraded = "degraded"
-    at_risk = "at risk"
+    """A set of pre-defined statuses for an incident"""
+
+    DOWN = "down"
+    RESOLVED = "resolved"
+    DEGRADED = "degraded"
+    AT_RISK = "at risk"
 
 
 class Update(BaseModel):
+    """An update contains further details and an optional URL for more info"""
+
     date: Annotated[str, AfterValidator(check_date_format)]
     details: str
     url: Optional[HttpUrl] = None
 
 
 class Incident(BaseModel):
+    """An incident contains details and a list of updates associated with it"""
+
     status: Status
     affectedServices: str
     summary: str
@@ -33,4 +40,6 @@ class Incident(BaseModel):
 
 
 class StatusPage(RootModel[Any]):
+    """The root of the status page object, containing a list of incidents"""
+
     root: list[Incident]
