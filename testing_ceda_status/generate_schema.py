a94e8358e728f195c45557f02339fc4c6c2ad3b5
danny-lloyd <danny.lloyd@stfc.ac.uk>
2024-10-04 16:29:53 +0100

    Use json.dump instead of directly writing json.dumps

--- testing_ceda_status/generate_schema.py
+++ testing_ceda_status/generate_schema.py
@@ -5,4 +5,4 @@ from model import StatusPage
 schema = StatusPage.model_json_schema()
 
 with open("generated_schema.json", "w") as out_file:
-    out_file.write(json.dumps(schema, indent=2))
+    json.dump(schema, out_file, indent=2)
