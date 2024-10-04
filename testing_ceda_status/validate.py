7e9db22c9ae6f20bd001d812311ed031dd51d4c7
danny-lloyd <danny.lloyd@stfc.ac.uk>
2024-10-04 15:46:57 +0100

    Use sys.exit() rather than exit()
    exit() assumes site modules are available

--- testing_ceda_status/validate.py
+++ testing_ceda_status/validate.py
@@ -1,11 +1,14 @@
+import sys
+
 from model import StatusPage
 from pydantic import ValidationError
 
 with open("status.json", "r") as status_file:
     status_page = status_file.read()
 
+# using a try/except block so that the traceback doesn't clog up pre-commit/CI output
 try:
     StatusPage.model_validate_json(status_page)
 except ValidationError as err:
     print(err)
-    exit(1)
+    sys.exit(1)
